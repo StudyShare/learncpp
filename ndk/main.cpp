466f2815dfb527febd514dd5c7551acd74a40068
shengyanming <shengyanming@altamob.com>
2019-04-28 20:08:39 +0800

    函数指针

--- ndk/main.cpp
+++ ndk/main.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+void testFunAddress(int a);
+
 int main() {
     printf("%s\n", "hello jni");
+    testFunAddress(3);
+    void (*funP)(int); //函数指针声明
+    void testFunAddress(int); //函数指针地址
+    funP = testFunAddress;//赋值
+    funP(10); //调用
+    (*funP)(100);//兼容
+
     return 0;
+}
+
+void testFunAddress(int a) {
+
+    printf("%d\n", a);
+    return;
 }
