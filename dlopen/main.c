2957c5de4c133f1e7e0ae558ba5f77a3f108d8a6
shengyanming <shengyanming@altamob.com>
2019-05-10 15:05:05 +0800

    测试dlopen

--- dlopen/main.c
+++ dlopen/main.c
@@ -6,7 +6,7 @@
 int main() {
     printf("Hello, World!\n");
     void (*func)();
-    while(1){
+    /*while(1){
         sleep(1);
 
         void *handler = dlopen("./Test.so", RTLD_LAZY);
@@ -21,6 +21,20 @@ int main() {
         }else{
             printf("dlopen - %s\n",dlerror());
         }
+    }*/
+
+    void *handler = dlopen("./Test.so", RTLD_LAZY);
+    handler = dlopen("./Test.so", RTLD_LAZY);
+    if (handler) {
+        func = dlsym(handler, "_printf");
+
+        if(func){
+            (*func)();
+        }else{
+            printf(" dlsym - %s\n",dlerror());
+        }
+    }else{
+        printf("dlopen - %s\n",dlerror());
     }
 
     return 0;
---
+++ dlopen/obj/local/arm64-v8a/objs/hook/main.o.d
@@ -0,0 +1,1 @@
+./obj/local/arm64-v8a/objs/hook/main.o: main.c
